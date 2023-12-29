70e90146fcde9af49f60985f1a0efb49a66c36da
akosilov <akosilov@inbox.ru>
2023-12-29 13:50:41 +0300

    order page

---
+++ src/components/OrderPayment.vue
@@ -0,0 +1,29 @@
+<script setup>
+import orderStore from '@/store/order'
+import { computed } from 'vue'
+const order = orderStore()
+const payments = computed(() => {
+  return order.payments
+})
+
+const orderPayment = computed({
+  get() {
+    return order.orderPayment
+  },
+  set(value) {
+    console.log(value)
+    order.orderPayment = value
+  }
+})
+</script>
+<template>
+  <h3 class="cart__title">Оплата</h3>
+  <ul class="cart__options options">
+    <li class="options__item" v-for="pay of payments" :key="pay.id">
+      <label class="options__label">
+        <input class="options__radio sr-only" type="radio" :value="pay.id" v-model="orderPayment" />
+        <span class="options__value"> {{ pay.title }} </span>
+      </label>
+    </li>
+  </ul>
+</template>
